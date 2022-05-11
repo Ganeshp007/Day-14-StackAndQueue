8c312cb9212a1c5e8798b51171cd464ddbc63631
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-11 20:40:44 +0530

    Added: UC1_CreateStackusingLinkedList

---
+++ Day-14-StackAndQueue/LinkedListStackOps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_14_StackAndQueue
+{
+    internal class LinkedListStackOps
+    {
+
+        private Node top;
+
+        internal void Push(int value)
+        {
+            Node node = new Node(value);
+            if (this.top == null)
+                node.next = null;
+            else
+                node.next = this.top;
+            this.top = node;
+            Console.WriteLine("\n>>> {0} pushed to stack...", value);
+        }
+
+        public void Display()
+        {
+            Node temp = this.top;
+            Console.WriteLine("\n>>> Stack-1 :- ");
+            while (temp != null)
+            {
+                Console.Write("\t\t| "+temp.data + " |\n");  
+                temp = temp.next;
+            }
+            
+        }
+
+
+
+    }
+}
---
+++ Day-14-StackAndQueue/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day_14_StackAndQueue;
+Console.WriteLine("----- Stack Using LinkedList -----");
+LinkedListStackOps linkedListStackOps = new LinkedListStackOps();
+linkedListStackOps.Push(70);
+linkedListStackOps.Push(30);
+linkedListStackOps.Push(56);
+linkedListStackOps.Display();
