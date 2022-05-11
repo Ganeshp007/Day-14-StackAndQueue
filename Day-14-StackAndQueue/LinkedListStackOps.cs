f0b2969dcac9d2721374d7a3ccd22cc0190f5db7
Ganeshp007 <potdart40.gp@gmail.com>
2022-05-11 21:19:26 +0530

    Added: UC2_PeekPopTillStackEmpty

--- Day-14-StackAndQueue/LinkedListStackOps.cs
+++ Day-14-StackAndQueue/LinkedListStackOps.cs
@@ -19,18 +19,63 @@ namespace Day_14_StackAndQueue
             else
                 node.next = this.top;
             this.top = node;
-            Console.WriteLine("\n>>> {0} pushed to stack...", value);
+            Console.WriteLine("> {0} pushed to stack...", value);
+        }
+
+
+        internal void Peek()
+        {  
+            if (this.top == null)
+            {
+                Console.WriteLine("\n>> Stack - 1 :-\n\t\t | Stack is Empty |\n");
+                return;
+            }
+            else
+            Console.WriteLine("> Peek :- {0} is in the top of the stack ", this.top.data);
+        }
+
+        internal void Pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("\n>> Stack - 1 :- Can not Delete Element\n\t\t | Stack is Empty |\n");
+                return;
+            }
+            Console.WriteLine("> Pop :- Value popped is {0} ", this.top.data);
+            this.top = this.top.next;
+        }
+
+        internal void IsEmpty()
+        {  
+            Console.WriteLine("\n----------------------------------\n>> Performing EmptyStack Task :-\n");
+            while (this.top != null)
+            {
+                Peek();
+                Pop();
+            }
+            Console.WriteLine("\n----------------------------------");
+
         }
 
         public void Display()
         {
-            Node temp = this.top;
-            Console.WriteLine("\n>>> Stack-1 :- ");
-            while (temp != null)
+            if(top == null)
             {
-                Console.Write("\t\t| "+temp.data + " |\n");  
-                temp = temp.next;
+                Console.WriteLine("\n >>> Stack - 1 :-\n\t\t  | Stack is Empty |\n");
             }
+            else
+            {
+                Node temp = this.top;
+                Console.WriteLine("\n>>> Stack-1 :- ");
+                while (temp != null)
+                {
+                    Console.Write("\t\t| " + temp.data + " |\n");
+                    temp = temp.next;
+                }
+
+                Console.WriteLine("\n");
+            }
+            
             
         }
 
